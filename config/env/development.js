cc27a56cb1e4f5287acb668e81adaa234431a666
Liang Xu <liangxu@nle-tech.com>
2017-10-11 19:06:52 +0800

    optimize env

--- config/env/development.js
+++ config/env/development.js
@@ -2,6 +2,5 @@ export default {
   session: 'secret-boilerplate-token',
   token: 'secret-jwt-token',
   database: 'mongodb://47.52.138.106:27017/novel',
-  // origin: 'http://www.0lx.top',
-  origin: 'http://localhost:3000',  
+  origin: 'http://localhost:3000',
 }
--- config/index.js
+++ config/index.js
@@ -1,6 +1,11 @@
-import common from './env/common'
-
-const env = process.env.NODE_ENV || 'development'
-const config = require(`./env/${env}`).default
+import common from './env/common';
+const env = process.env.NODE_ENV || 'production';
+let config
+if (env === 'production') {
+  config = require(`./env/production`).default;
+} else {
+  config = require(`./env/development`).default;
+}
+console.log(env);
 
 export default Object.assign({}, common, config)
